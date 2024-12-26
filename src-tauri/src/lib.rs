f92e1d6ad0d7a8be25d0a890225d8248b9408199
ItsAlbertZhang <itsalbertzhang@gmail.com>
2024-12-26 16:04:25 +0800

    init: rust crates

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,16 +1,18 @@
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
-  tauri::Builder::default()
-    .setup(|app| {
-      if cfg!(debug_assertions) {
-        app.handle().plugin(
-          tauri_plugin_log::Builder::default()
-            .level(log::LevelFilter::Info)
-            .build(),
-        )?;
-      }
-      Ok(())
-    })
-    .run(tauri::generate_context!())
-    .expect("error while running tauri application");
+    println!("The answer is {}.", add::add(40, 2));
+    println!("{}", hello::say_hello("jx3calc tauri".to_string()));
+    tauri::Builder::default()
+        .setup(|app| {
+            if cfg!(debug_assertions) {
+                app.handle().plugin(
+                    tauri_plugin_log::Builder::default()
+                        .level(log::LevelFilter::Info)
+                        .build(),
+                )?;
+            }
+            Ok(())
+        })
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
 }
---
+++ src/hello/src/main.rs
@@ -0,0 +1,3 @@
+fn main() {
+    println!("{}", hello::say_hello("jx3calc".to_string()));
+}
