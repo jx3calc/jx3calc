904d3406e0ded1a64b69c072120e21824d01716b
ItsAlbertZhang <itsalbertzhang@gmail.com>
2024-12-26 19:11:19 +0800

    doc: update README.md and fix a bug

--- src/pak/src/main.rs
+++ src/pak/src/main.rs
@@ -1,6 +1,6 @@
 fn main() {
     let name = std::ffi::CString::new("jx3calc").unwrap();
-    let message_ptr = hello::say_hello(name.as_ptr());
+    let message_ptr = pak::say_hello(name.as_ptr());
     let message = unsafe { std::ffi::CStr::from_ptr(message_ptr).to_str().unwrap() };
     println!("{}", message);
 }
