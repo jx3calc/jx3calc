4b71a9504fb2a2012da174b39dee81d761afebca
ItsAlbertZhang <itsalbertzhang@gmail.com>
2024-12-27 16:07:38 +0800

    feat: 初步定义pak接口

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,10 +1,10 @@
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     println!("The answer is {}.", add::add(40, 2));
-    let name = std::ffi::CString::new("jx3calc tauri").unwrap();
-    let message_ptr = pak::say_hello(name.as_ptr());
-    let message = unsafe { std::ffi::CStr::from_ptr(message_ptr).to_str().unwrap() };
-    println!("{}", message);
+    // let name = std::ffi::CString::new("jx3calc tauri").unwrap();
+    // let message_ptr = pak::say_hello(name.as_ptr());
+    // let message = unsafe { std::ffi::CStr::from_ptr(message_ptr).to_str().unwrap() };
+    // println!("{}", message);
     tauri::Builder::default()
         .setup(|app| {
             if cfg!(debug_assertions) {
