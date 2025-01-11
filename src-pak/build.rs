7df1111cfae3d33a1c2e37f6f56c0c79c91ed06a
Albert Zhang <93331929+ItsAlbertZhang@users.noreply.github.com>
2025-01-11 22:01:08 +0800

    ci: github workflow (#12)

--- src-pak/build.rs
+++ src-pak/build.rs
@@ -1,13 +1,38 @@
 use std::path::Path;
 
 fn main() {
-    let lib_dir = Path::new(file!()).parent().unwrap().join("lib");
-    let repo_dir = Path::new("src/jx3pak");
-    if repo_dir.exists() {
+    let dir_current = std::env::current_dir().unwrap();
+    let dir_lib = dir_current.join("lib");
+    let dir_repo = Path::new("src/jx3pak");
+    if dir_repo.exists() {
         println!("cargo:rustc-cfg=feature=\"lib_local\"");
-    } else {
-        println!("cargo:rustc-link-lib=dylib=pak");
-        println!("cargo:rustc-link-search=native={}", lib_dir.display());
-        println!("cargo:rustc-env=DYLD_LIBRARY_PATH={}", lib_dir.display());
+        return;
     }
+
+    let import = if cfg!(target_os = "windows") {
+        "pak.dll"
+    } else {
+        "pak"
+    };
+    println!("cargo:rustc-link-lib=dylib={}", import);
+    println!("cargo:rustc-link-search=native={}", dir_lib.display());
+    // if cfg!(target_os = "linux") || cfg!(target_os = "macos") {
+    //     println!("cargo:rustc-link-arg=-Wl,-rpath,./");
+    // }
+    // Copy dynamic library to OUT_DIR
+    let dir_out = std::env::var("OUT_DIR").unwrap();
+    let dir_target = Path::new(&dir_out);
+    let dir_target = dir_target.parent().unwrap();
+    let dir_target = dir_target.parent().unwrap();
+    let dir_target = dir_target.parent().unwrap();
+    let fn_lib = if cfg!(target_os = "windows") {
+        "pak.dll"
+    } else if cfg!(target_os = "macos") {
+        "libpak.dylib"
+    } else {
+        "libpak.so"
+    };
+    let path_src = dir_lib.join(fn_lib);
+    let path_dest = dir_target.join(fn_lib);
+    std::fs::copy(&path_src, &path_dest).unwrap();
 }
